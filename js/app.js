79eaaa0299abf273c041afcb5f25d651bb10bcc3
Tim Stevenson <t.stevenson33@gmail.com>
2014-02-10 18:02:35 -0500

    added content to ux page

--- js/app.js
+++ js/app.js
@@ -2,13 +2,16 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 $(document).ready(function() {
-	$.fn.fullpage({
-  	navigation: false,
-  	navigationPosition: 'left',
-  	css3: true,
-  	anchors: ['bobby','work','resume','contact']
-	});
-});
+  if($(".fullPage").length) {
+   	$.fn.fullpage({
+    	navigation: false,
+    	navigationPosition: 'left',
+    	css3: true,
+    	anchors: ['bobby','work','resume','contact']
+  	});
+  }
+}); 
+  
 $(".off-canvas-wrap").on("click", ".off-canvas-list li a", function(e){
   $(".off-canvas-wrap").removeClass("move-left");
 });
