fa6061c408bf5ae2d994af7ef5da54012d045fd5
timihendrix33 <t.stevenson33@gmail.com>
2016-01-08 00:17:35 -0500

    fixing css compiling, js issues

--- incl/scripts.php
+++ incl/scripts.php
@@ -1,4 +1,4 @@
-    <script src="bower_components/jquery/jquery.js"></script>
+    <script src="bower_components/jquery/dist/jquery.js"></script>
     <script src="bower_components/foundation/js/foundation.min.js"></script>
     <script src="js/app.js"></script>
     <script src="js/jquery-2.1.0.min.js" type="text/javascript"></script>
